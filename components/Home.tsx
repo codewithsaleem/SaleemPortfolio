42947e85051c240b9fddc7a3dbaad5d02e1688d0
Mohd Saleem <95497722+codewithsaleem@users.noreply.github.com>
2024-10-02 17:20:30 +0530

    Update Home.tsx

--- components/Home.tsx
+++ components/Home.tsx
@@ -17,7 +17,7 @@ const Home = () => {
             <div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
                 <div>
                     <h1 className='text-[35px] mt-[2rem] md:text-[50px] text-white font-bold'>
-                        HI, I'M <span className='text-yellow-400'>MOHD SALEEM!</span>
+                        HI, I AM <span className='text-yellow-400'>MOHD SALEEM</span>
                     </h1>
                     <TextEffect />
                     <p className='mt-[2rem] text-[18px] text-[#ffffff92]'>
